6eb77048f37687bb0a2cd42e8c163c7e09f9d62c
Eric <edown@mit.edu>
2020-05-14 17:02:07 -0400

    adding example 1.1 to test; streamlined function calls

--- ch1.py
+++ ch1.py
@@ -1,12 +1,6 @@
-'''
-0. test what we have so far
-1. we are implementing a hypergraph here...
-any hypergraph can be represented as a bipartite graph
-so we should make those concepts explicit & accessible
-'''
-
 from collections import defaultdict
 from functools import reduce
+from typing import *
 import warnings
 
 class NDRelation:
@@ -25,29 +19,37 @@ class NDRelation:
                 emaps[e].add(sid)
                 dmaps[d].add(sid)
         self.objects = frozenset(objs)
-        self._emaps = emaps
-        self._dmaps = dmaps
-        self._imaps = imaps
-
-    # TDB is using set-valued functions so ea image is also a point
-    def images(e, rm_self:bool = True, check_memb:bool = True):
-        if check_memb and e not in self.objects: return set()
-        rm = {e} if rm_self else set()
-        return {s - rm for sid in self._emaps.get(e, dict()) for s in self._imaps[sid]}
+        self._emaps = emaps # object -> set of sid's
+        self._dmaps = dmaps # rank -> set of sid's
+        self._imaps = imaps # sid -> set of objects
         
-    def fiber(e, U:bool = True, rm_self:bool = True, check_memb:bool = True):
-        if check_memb and e not in self.objects: return set()
+    # TDB is using set-valued functions so ea image is also a set
+    # each element of these is the image of a morphism in Set
+    def images(self, e, rm_self:bool = True):
+        rm = {e} if rm_self else set()
+        return {s - rm for sid in self._emaps[e] for s in self._imaps[sid]}
+        
+    def fiber(self, e, U:bool = True, rm_self:bool = True):
         f = set.union if U else set.intersection
         rm = {e} if rm_self else set()
-        return f( *{*self._imaps[sid] for sid in self._emaps[e]} ) - rm
+        return f( *self.images(e, rm_self=False) ) - rm
 
-    # TDB's "extension f(A)" is preimage(A, U = False)
-    # we remove self at the scale of a fiber
-    def preimage(sub:set, U:bool = True, rm_self:bool = True): 
-        sub = sub.intersection(self.objects)
+    def preimage(self, sub:set, U:bool = True, rm_self:bool = True): 
+        sub = sub.intersection(self.objects) # otherwise if U risk false {} preimage
         f = set.union if U else set.intersection
-        return f( *{self.fiber(e, U, rm_self, False) for e in sub} )
+        rm = sub if rm_self else set()
+        return f( *{self.fiber(e, U, False) for e in sub} ) - rm
 
+    # TDB's "extension f(A) of a(x)" is preimage(A, U = False)
+    def extension(self, sub:set, rm_self:bool = True): 
+        return self.preimage(sub, U=False, rm_self)
+
+    def relations(self):
+        return self._imaps.values()
+
+    def table(self):
+        pass
+    
     def test_self(full = False):
         R = {{1,2}, {1,2,3}, {1,3}, {3,4}}
         ndrel = NDRelation(R)
@@ -55,14 +57,16 @@ class NDRelation:
         assert(ndrel.images(1) == {{2}, {2,3}, {3}})
         assert(ndrel.fiber(1) == {2,3})
         assert(ndrel.preimage({3,4}) == {2,3,4})
-        if full:
-            old = set()
-            for new in map(set, powerlist(list(ndrel.objects))):
-                X = old.issubset(ndrel.preimage(new))
-                Y = new.issubset(ndrel.preimage(old))
-                assert(x == Y)
-                old = new
-            
+        assert(ndrel.extension({2,3}) == set())
+        assert(ndrel.extension({2}) == {1})
+
+        R = {{'orange','fruit'}, {'green','fruit'}, {'purple','vegetable'}}
+        ndrel = NDRelation(R)
+        assert(ndrel.images('fruit') == {{'orange'}, {'green'}})
+        assert(ndrel.extension({'orange','green'}) == {'fruit'})
+        assert(ndrel.extension({'purple'}) == {'vegetable'})
+
+        
 # if empty sets commonly result from fiber and preimage with U=False,
 # use intersect() instead of set.intersection() for speed improvement
 def intersect(*args):
@@ -72,18 +76,32 @@ def intersect(*args):
 def cap(x:set, y:Sequence, raize:bool=True):
     x.intersection_update(y) 
     if raize and not x: raise Exception() 
-    return x 
+    return x
 
 def powerlist(seq:list):
     if len(seq) <= 1:
         yield seq
         yield []
     else:
-        for sub in powerlist(s[1:]):
+        for sub in powerlist(seq[1:]):
             yield [seq[0]] + sub
             yield sub
-    
+
+def concept_generator(self, seq=list(self.objects)):
+    if len(seq) > 1:
+        for sub in concept_search(seq[1:]):
+            x = [seq[0]] + sub
+            if self.extension(set(x))
+            yield [seq[0]] + sub
+            yield sub
+
+            
 '''
+0. test what we have so far
+1. we are implementing a hypergraph here...
+any hypergraph can be represented as a bipartite graph
+so we should make those concepts explicit & accessible
+
 los = [*[{1}]*10, {}, *[{1}]*900] 
 %timeit set.intersection(*los)
     35.4 µs ± 64.5 ns per loop (mean ± std. dev. of 7 runs, 10000 loops each)
