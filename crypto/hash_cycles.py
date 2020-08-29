68cd0bc5c84c3c4d250b09141bd3ce216701d616
Eric <edown@mit.edu>
2020-08-29 06:44:18 -0400

    corrected typo; reduced mem overhead

--- crypto/hash_cycles.py
+++ crypto/hash_cycles.py
@@ -4,7 +4,7 @@ lets study the endomorphisms of cryptographic hash functions!
 TODO:
  * is hashlib.sha3_256 the same as keccak 256 in Monero?, eth?
  * use graphviz to make pretty graphs from the edge dict
- * make an interruptible version that pickles state uppon SIGHUP, SIGTERM, etc
+ * make an interruptible version that pickles state upon SIGHUP, SIGTERM, etc
 '''
 
 import os
@@ -17,7 +17,7 @@ from functools import partial
 
 if not os.path.isdir('hendo'): os.mkdir('hendo')
 
-def trycept(f, *rgs, default = None, raize = False, **nargs):
+def trycept(f, *args, default = None, raize = False, **nargs):
     try: f(*args, **nargs)
     except Exception as e:
         if raize: raise e
@@ -28,14 +28,15 @@ def blake(x:bytes, nbytes:int) -> bytes:
 
 def edge(x:int, f:Callable[bytes,bytes]):
     x = x.to_bytes(L,'big')
-    return {x: f(x)}
+    return (x, f(x))
 
 def gen_edges(L:int) -> Dict[bytes,bytes]:
     with mp.Pool(mp.cpu_count() * 3 // 4) as p:
         g = partial(edge, f = partial(blake, nbytes = L))
         edges = p.map(g, range(2 ** L))
-        pickle.dump(edges, f'hendo/{L}_full.pkl')
-        return edges
-                   
+        edge_dict = {t[0]:t[1] for t in edges}
+        pickle.dump(edge_dict, f'hendo/{L}_full.pkl')
+        return edge_dict
+
 if __name__ == '__main__' and trycept(not_, __IPYTHON__):
     gen_edges(sys.argv[1] if len(sys.argv) > 1 else 1)
