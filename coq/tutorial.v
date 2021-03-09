7a32a826157ff725853a2253d4990c7ffca96ae4
Eric Downes <eric@triskew.com>
2021-03-09 05:13:54 -0500

    added bullets; now works with HoTT

--- coq/tutorial.v
+++ coq/tutorial.v
@@ -1,29 +1,35 @@
-(* 
-Instructions for OSX
+(* Instructions for OSX
 
-1. `brew install coq` 
-2. follow instructions linked at
-   https://mdnahas.github.io/doc/nahas_tutorial
+1. `brew install coq` 2. follow instructions linked at
+https://mdnahas.github.io/doc/nahas_tutorial
 
 this gives you the basic coqtop.  For groupoid (instead of setoid)
 based reasoning, use the HoTT version of coqtop:
 
-3. if in emacs, add the line `(setq proof-prog-name-ask 1)` to `~/.emacs`
-4. clone hott library repo:
+3. if in emacs, add the line `(setq proof-prog-name-ask 1)` to
+`~/.emacs` 4. clone hott library repo:
 
 `git clone https://github.com/HoTT/HoTT.git`
 
-5. install
+5. install to system
 
-```
-cd HoTT
-./autogen.sh
-./configure
-make
-```
+``` cd HoTT ./autogen.sh ./configure make ```
+
+6. Add `hoqtop` to your path, e.g.
+
+`export PATH=$PATH:/path/to/hoqtop`
+
+7. At the top of a file you want to use HoTT tools:
+
+`Require Import HoTT.`
+
+For more, see: https://github.com/HoTT/HoTT/blob/master/INSTALL.md
+
+Notes on HoTT: 
+ -- you need to include bullets when (- + *, -- ++ **, etc) when using subgoals
 
 *)
-
+Require Import HoTT.
 
 (* True -> True *)
 Theorem my_first_proof : (forall A : Prop, A -> A).
@@ -56,12 +62,18 @@ Proof.
   intros A B C.
   intros pA AiB AiBiC.
   refine (AiBiC _ _).
-    exact pA.
-    refine (AiB _).
-      exact pA.
+  - exact pA.
+  - refine (AiB _).
+    + exact pA.
 Show Proof.
 Qed.
+(* these bullets -,+ are good formatting practice, but required in HoTT/Coq 
+http://prl.ccs.neu.edu/blog/2017/02/21/bullets-are-good-for-your-coq-proofs/
+*)
 
+
+
+  
 (* 
 stopped at "true and false vs. True and False" 
 https://mdnahas.github.io/doc/nahas_tutorial
