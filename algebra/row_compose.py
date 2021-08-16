2c3f96d9fffab4895cd58bdf89dd456ae762ae14
Eric Downes <eric@triskew.com>
2021-08-16 01:13:34 -0400

    fixed minor issue

--- algebra/row_compose.py
+++ algebra/row_compose.py
@@ -1,28 +1,29 @@
-
+from __future__ import annotations
+from typing import *
 
 
 class Endo(list):
     def __init__(self, l:List[int]):
-        super().__init__([i for i in l if isinstance(i, int)])
+        super().__init__(l)
         self.is_id = self == list(range(len(self)))
         
     def __mul__(self, other:Union[int, List[int]]) -> Endo:
         return self(other)
-        
-    def __call__(self, other:Union[int, List[int]], strict:bool = True) -> Endo:
-        if isinstance(other, int):
-            return self[other % len(self)]
-        if strict:
-            assert len(self) == len(other)
-        if self.is_id:
-            return other
-        if isinstance(other, Endo) and other.is_id:
-            return self
-        return [self[i % len(self)] for i in other]
+
+    def __call__(self, other:Union[int, List[int]], strict:bool = True) -> Union[Endo, list, int]:
+        if isinstance(other, int): return self[other % len(self)]
+        if strict: assert len(self) == len(other)
+        if self.is_id: return other
+        if isinstance(other, Endo) and other.is_id: return self
+        x = [self[i % len(self)] for i in other]
+        if isinstance(other, list): return x
+        if isinstance(other, Endo): return Endo(x)
         
     def __rmul__(self, other:List[int]) -> Endo:
-        return Endo(other)(self)
+        if isinstance(other, list): return Endo(other)(self)
+        if isinstance(other, Endo): return other(self)
 
+    
 class Coset(set):
     def __init__(self, *args, op:Binop = None, **kwargs):
         super().__init__(*args, **kwargs)
@@ -31,5 +32,3 @@ class Coset(set):
     def __mul__(self, x:Any):
         return {self.op(x,y) for y in self}
         
-
-    
